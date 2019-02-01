501ae212e191fcec303a462301c40ea7cd39786b
Goldensnake <juliendom.sacre@gmail.com>
2019-02-01 23:23:06 +0100

    add timber log lib

--- app/src/main/java/com/juliensacre/rickmortydating/App.kt
+++ app/src/main/java/com/juliensacre/rickmortydating/App.kt
@@ -4,15 +4,11 @@ import android.app.Application
 import timber.log.Timber
 
 
-
 class App : Application(){
 
     override fun onCreate() {
         super.onCreate()
 
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-
+        Timber.plant(Timber.DebugTree())
     }
 }
