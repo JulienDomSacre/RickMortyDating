7fc73c0e2355ed1bfd9169bd2e075a09b3a01219
Goldensnake <juliendom.sacre@gmail.com>
2019-02-02 17:38:58 +0100

    define the UI for character detail

--- app/src/main/java/com/juliensacre/rickmortydating/characterdetail/CharacterDetailFragment.kt
+++ app/src/main/java/com/juliensacre/rickmortydating/characterdetail/CharacterDetailFragment.kt
@@ -4,13 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.juliensacre.rickmortydating.R
+import com.juliensacre.rickmortydating.data.Character
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_character_detail.*
 
 class CharacterDetailFragment : Fragment() {
+    companion object {
+        private const val CHARACTER_ID = "characterId"
+        fun newInstance(characterId : Int) = CharacterDetailFragment().apply {
+            arguments = bundleOf(
+                CHARACTER_ID to characterId)
+        }
+    }
 
+    private lateinit var viewModel : CharacterDetailViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        //todo init view
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_character_detail, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = activity?.run {
+            ViewModelProviders.of(this).get(CharacterDetailViewModel::class.java)
+        } ?: throw Exception("Invalid Activity")
+
+        val stringId = arguments!!.getInt(CHARACTER_ID).toString()
+
+        viewModel.getCharacter(stringId.toInt()).observe(this, Observer { bindData(it) })
+
+        activity to AppCompatActivity() .supportActionBar
+    }
+
+    private fun bindData(character : Character){
+        with(character){
+            Picasso.with(this@CharacterDetailFragment.context).load(image).into(photo)
+            status_text.text = status
+            origin_text.text = origin.name
+            species_text.text = species
+            gender_text.text = gender
+            seen_text.text = location.name
+        }
+
+
     }
 }
--- app/src/main/java/com/juliensacre/rickmortydating/characterdetail/CharacterDetailViewModel.kt
+++ app/src/main/java/com/juliensacre/rickmortydating/characterdetail/CharacterDetailViewModel.kt
@@ -1,8 +1,41 @@
 package com.juliensacre.rickmortydating.characterdetail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.juliensacre.rickmortydating.data.Character
+import com.juliensacre.rickmortydating.data.source.remote.ApiClient
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 import javax.inject.Inject
 
 class CharacterDetailViewModel @Inject constructor() : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+    private val character = MutableLiveData<Character>()
 
+    fun getCharacter(id : Int) : LiveData<Character> {
+        //Timber.d("getCharacters call for nextPage $nextPage")
+        val charactersDisposable = ApiClient.getCharacterService().getCharacter(id)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    character.value = it
+                },
+                {
+                    //todo error
+                }
+            )
+
+        compositeDisposable.add(charactersDisposable)
+        return character
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+        Timber.d("!!! cleared !!! ")
+    }
 }
