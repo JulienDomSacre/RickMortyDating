e8475fb0e63586963f4bd014ddc14935bac88a40
Goldensnake <juliendom.sacre@gmail.com>
2019-03-06 12:45:48 +0100

    implement navigation and toolbar in activity

--- app/src/main/java/com/juliensacre/rickmortydating/ui/MainActivity.kt
+++ app/src/main/java/com/juliensacre/rickmortydating/ui/MainActivity.kt
@@ -2,14 +2,22 @@ package com.juliensacre.rickmortydating.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.juliensacre.rickmortydating.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
     }
 
     override fun onSupportNavigateUp(): Boolean = findNavController(R.id.nav_host_fragment).navigateUp()
